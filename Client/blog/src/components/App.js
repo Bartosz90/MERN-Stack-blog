cc71614d961e1ad85dd5a547e1eef94c5a7b28b5
Bartek Krupa <krupa1.bk@gmail.com>
2019-09-06 15:46:20 +0200

    initial commit

---
+++ Client/blog/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/app.sass";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world! Again!</h1>
+    </div>
+  );
+}
+
+export default App;
